83c49f617dbd65a148a0847ea8b34779a2a95712
PlusTM <telekingzarin@gmail.com>
2017-09-18 14:56:51 -0700

    Setup Packages

---
+++ Setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+echo -e "Setup Started Please Wait..."
+sleep 2
+sudo apt-get install python-pip
+pip install pyTelegramBotAPI
+git clone https://github.com/eternnoir/pyTelegramBotAPI.git
+cd pyTelegramBotAPI
+python setup.py install
+cd ..
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+sudo apt-get install python-redis
+sudo pip install pyTelegramBotAPI
+apt-get install python setup-tools
+sudo pip install pyTelegramBotAPI —upgrade
+sudo apt-get update
+sudo apt-get install python2.7
+chmod +x Anti.sh
+echo -e "Thanks for Used AntiPlus"
+sleep 2
+echo -e "Please Set TOKEN and Sudo ID To AntiPlus.py"
+sleep 3
+echo -e "Type screen ./Anti.sh"
